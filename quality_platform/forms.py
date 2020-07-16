ce4eb79952891e570d0a5d168285a92964ad1dec
liuhz0926 <liu.h0926@gmail.com>
2020-07-16 17:59:26 -0400

    Delete all

--- quality_platform/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import EvalPredFile
-
-class EvalFileForm(forms.ModelForm):
-    class Meta:
-        model = EvalPredFile
-        fields = ('truth_file', 'prediction_file')
